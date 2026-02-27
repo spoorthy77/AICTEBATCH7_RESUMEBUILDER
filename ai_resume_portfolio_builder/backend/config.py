3bd88c85e9aca35db1eff8b098714b166b282de6
spoorthy77 <kmspoorthi1@gmail.com>
2026-02-27 17:08:17 +0530

    Remove old project structure files

--- ai_resume_portfolio_builder/backend/config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "secret_key_here")
-    GROQ_API_KEY = os.getenv("GROQ_API_KEY", "")
